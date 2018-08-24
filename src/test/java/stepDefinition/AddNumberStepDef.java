c57f26f48c1c6a24576bbe4b2a5eb915d94f3fae
Aziiz PC <aziizpc@ymail.com>
2018-08-24 15:01:08 +0530

    Test

---
+++ src/test/java/stepDefinition/AddNumberStepDef.java
@@ -0,0 +1,29 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Then;
+
+public class AddNumberStepDef 
+{	
+	int sum;
+	
+	@Then("^User add this two number$")
+	public void user_add_this_two_number() throws Throwable 
+	{
+		sum = 100+300;
+	}
+	
+	@Then("^User add this three number$")
+	public void user_add_this_three_number() throws Throwable 
+	{
+		sum = 123+400+300;
+	}
+	
+	@Then("^Print the sum$")
+	public void print_the_sum() throws Throwable 
+	{
+		System.err.println("Sum is  :"+sum);
+	}
+
+}
+
+//Test
